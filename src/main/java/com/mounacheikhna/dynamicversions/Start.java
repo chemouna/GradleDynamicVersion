8a315bcf1ec5fbd46bf944aaf50927091f76475a
Mouna Cheikhna <cheikhnamouna@gmail.com>
2018-03-22 16:11:19 +0000

    A working version with one lib version.

--- src/main/java/com/mounacheikhna/dynamicversions/Start.java
+++ src/main/java/com/mounacheikhna/dynamicversions/Start.java
@@ -1,9 +1,10 @@
 package com.mounacheikhna.dynamicversions;
 
+import com.github.chemouna.dummylib.DummyVersion;
+
 public class Start {
 
     public static void main(String[] args) {
-        System.out.println("Start");
-
+        System.out.println(new DummyVersion().getVersion());
     }
 }
